1d63b1ca1befe0718bc0d103d23fda8e18131f5f
Source Code Examples <74339793+sourcecodeexamples@users.noreply.github.com>
2021-12-19 09:46:03 +0530

    Update EmployeeDAOJpaImpl.java

--- 11-spring-boot-rest-crud-with-jpa/22-jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ 11-spring-boot-rest-crud-with-jpa/22-jpa-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -22,60 +22,35 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
 	
 	@Override
 	public List<Employee> findAll() {
-
 		// create a query
-		Query theQuery = 
-				entityManager.createQuery("from Employee");
-		
+		Query theQuery = entityManager.createQuery("from Employee");		
 		// execute query and get result list
-		List<Employee> employees = theQuery.getResultList();
-		
+		List<Employee> employees = theQuery.getResultList();		
 		// return the results		
 		return employees;
 	}
 
 	@Override
 	public Employee findById(int theId) {
-
 		// get employee
-		Employee theEmployee = 
-				entityManager.find(Employee.class, theId);
-		
+		Employee theEmployee = entityManager.find(Employee.class, theId);		
 		// return employee
 		return theEmployee;
 	}
 
 	@Override
 	public void save(Employee theEmployee) {
-
 		// save or update the employee
-		Employee dbEmployee = entityManager.merge(theEmployee);
-		
+		Employee dbEmployee = entityManager.merge(theEmployee);		
 		// update with id from db ... so we can get generated id for save/insert
-		theEmployee.setId(dbEmployee.getId());
-		
+		theEmployee.setId(dbEmployee.getId());		
 	}
 
 	@Override
 	public void deleteById(int theId) {
-
 		// delete object with primary key
-		Query theQuery = entityManager.createQuery(
-							"delete from Employee where id=:employeeId");
-		
-		theQuery.setParameter("employeeId", theId);
-		
+		Query theQuery = entityManager.createQuery("delete from Employee where id=:employeeId");		
+		theQuery.setParameter("employeeId", theId);		
 		theQuery.executeUpdate();
 	}
-
 }
-
-
-
-
-
-
-
-
-
-
